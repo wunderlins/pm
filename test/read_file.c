a32a0674d9d389c942e1e09ef7c77424c4f4f69c
wunderlins <swunderlin@gmail.com>
2013-03-24 10:14:31 +0100

    added

---
+++ test/read_file.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <getopt.h>
+#include <stdio.h>
+
+/** read a file into memmory
+ *
+ *
+ * // example:
+ * char* content;
+ * read_file("pm.c", &content);
+ * printf("%s", content);
+ */
+int read_file(char* filename, char** contents) {
+	int size = 0;
+
+	struct stat sb;
+
+	// file reachable and exists ?
+	printf("%s\n", filename);
+	if (stat(filename, &sb) == -1)
+		return errno;
+
+	// file must be readable by this user
+	FILE* fd;
+	fd = fopen(filename, "r");
+
+	// failed to open file
+	if (fd == NULL)
+		return errno;
+
+	// get file length
+	fseek(fd, 0, SEEK_END);
+	size = ftell(fd);
+	fseek(fd, 0, SEEK_SET);
+
+	// allocate memmory
+	*contents = (char*) malloc((char) sizeof(char)*sb.st_size + 1);
+	if (*contents == NULL) {
+		fclose(fd);
+		return errno;
+	}
+	printf("%d\n", (int) sb.st_size);
+
+	// check if the read size equals the file size
+	if (size != fread(*contents, sizeof(char), sb.st_size, fd)) {
+		free(*contents);
+		fclose(fd);
+		return 3; // -2 means file reading fail
+	}
+
+	// close handle
+	fclose(fd);
+
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+
+	char *contents;
+	int r = read_file("test_pypm.py", &contents);
+	printf("%d, %s\n", r, contents);
+
+	return 0;
+
+}
