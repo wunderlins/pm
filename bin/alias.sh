5897e394638effdfd83025552f47282a5c556418
wus <swunderlin@gmail.com>
2013-04-27 21:37:18 +0200

    fixed alias

--- bin/alias.sh
+++ bin/alias.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 alias ad='git add'
-alias co='git commit -m'
+alias co='git commit -a -m'
 alias diff='git diff | colordiff | less -R'
 alias psh='git push'
 alias pul='git pull'
